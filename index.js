b58533945470e761bec618b8f5576e9bf40383d4
Freyzzer Valencia <freyzzergvm12@gmail.com>
2023-11-25 21:02:28 -0500

    Finalizado

--- index.js
+++ index.js
@@ -1,13 +1,58 @@
 const btn = document.querySelector('.btn');
-
+const form_1 = document.getElementById('f1');
+const form_2 = document.querySelector('.cont-section2');
+let category = [];
 var l = localStorage;
 
-btn.addEventListener('click', function(){
-    const form_1 = document.querySelector('.form1');
-    const form_2 = document.querySelector('.cont-section2');
+function updateUser(){
+    category = ["category1","category2","category3"].map((id) => {
+        const checkbox = document.getElementById(id);
+        if(checkbox.checked)
+        {
+            return checkbox.value;
+        }
+        return null;
+    }).filter((value) => value !== null);
+}
 
-    form_1.addclass('desactivado');
-})
+function updateStepIndicator(step) {
+    const stepIndicator = document.getElementById("step-counter-indicator");
+    stepIndicator.innerHTML = "";
+    for (let i = 1; i <= 3; i++) {
+        const circle = document.createElement("div");
+        circle.classList.add("circle");
+        if (i < step) {
+            circle.classList.add("status-completed");
+        } else if (i === step) {
+            circle.classList.add("status-completed");
+            circle.classList.add("status-completed-active");
+        }
+        stepIndicator.appendChild(circle);
+    }
+}
+
+function esconder(event, step){
+    event.preventDefault();
+    updateUser();
+    document.getElementById("step" + (step - 1)).classList.add("desactivado");
+    document.getElementById("step" + (step)).classList.remove("desactivado");
+
+    const stepCounter = document.getElementById("step-number");
+    stepCounter.innerText = step;
+
+    updateStepIndicator(step);
+
+    if(step == 3)
+    {
+        const summaryN = document.getElementById("summary-name");
+        summaryN.innerHTML = l.getItem("fn");
+        const summaryE = document.getElementById("summary-email");
+        summaryE.innerHTML = l.getItem("em");
+        const summaryC = document.getElementById("summary-category");
+        summaryC.innerHTML=category.map((category) => `<li>${category}</li>`).join(" ");
+    }
+}
+
 
 const email = document.getElementById('GET-email')
 const first_name = document.getElementById('GET-name');
@@ -20,4 +65,9 @@ first_name.addEventListener("focusout", function() {
 email.addEventListener("focusout", function() {
     l.setItem("em", email.value);
 })
+
+
+
+updateStepIndicator(1);
+
 
